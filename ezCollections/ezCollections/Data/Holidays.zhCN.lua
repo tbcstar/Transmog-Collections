4d8c20f4e25900e5aa652c1efc88578e37f26ff0
yuanf225 <admin@itpel.com>
2022-10-21 22:30:34 +0800

    Update language
    Remove ruRU and add zhCN

---
+++ ezCollections/ezCollections/Data/Holidays.zhCN.lua
@@ -0,0 +1,34 @@
+-- Load localization only if it matches the client locale
+if GetLocale() ~= "zhCN" then
+    return;
+end
+
+ezCollections.Holidays =
+{
+    [62] = "Fireworks Spectacular",
+    [141] = "Feast of Winter Veil",
+    [181] = "Noblegarden",
+    [201] = "Children's Week",
+    [283] = "Call to Arms: Alterac Valley",
+    [284] = "Call to Arms: Warsong Gulch",
+    [285] = "Call to Arms: Arathi Basin",
+    [301] = "Stranglethorn Fishing Extravaganza",
+    [321] = "Harvest Festival",
+    [324] = "Hallow's End",
+    [327] = "Lunar Festival",
+    [335] = "Love is in the Air",
+    [341] = "Midsummer Fire Festival",
+    [353] = "Call to Arms: Eye of the Storm",
+    [372] = "Brewfest",
+    [374] = "Darkmoon Faire",
+    [375] = "Darkmoon Faire",
+    [376] = "Darkmoon Faire",
+    [398] = "Pirates' Day",
+    [400] = "Call to Arms: Strand of the Ancients",
+    [404] = "Pilgrim's Bounty",
+    [406] = "Wrath of the Lich King Launch",
+    [409] = "Day of the Dead",
+    [420] = "Call to Arms: Isle of Conquest",
+    [423] = "Love is in the Air",
+    [424] = "Kalu'ak Fishing Derby",
+};
