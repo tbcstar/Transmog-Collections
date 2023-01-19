d4fc6b812ec0dd78cef03a31a265b7964a114868
yuanf225 <admin@itpel.com>
2023-01-19 21:45:20 +0800

    取消混淆加密

--- ezCollectionsServer/ezCollectionSetting.lua
+++ ezCollectionsServer/ezCollectionSetting.lua
@@ -5,7 +5,7 @@ local ezCollections = {};
 --#######################################################
 
 --插件版本号,用于验证玩家客户端的版本号,一般不要改
-ezCollections.SERVERVERSION = 2.4.3;
+ezCollections.SERVERVERSION = 2.4.4;
 
 --插件缓存版本号,只要改动过物品的数据,需要更改这里的数值
 --只要玩家的客户端里缓存的数据版本和这里不一样,就会强制给玩家更新缓存
@@ -32,8 +32,8 @@ ezCollections.DEVELOPERMODE = false;
 
 --查询数据库任务表的奖励物品信息,用于给藏品加上一个任务来源的标签,类似的标签还有boss掉落,世界掉落,商人售卖等,实际没什么用,只是用来看的
 --如不想启用就留空
---ezCollections.QueryQuestRewardItemText = "SELECT id, RewardItemId1, RewardItemId2, RewardItemId3, RewardItemId4, RewardChoiceItemId1, RewardChoiceItemId2, RewardChoiceItemId3, RewardChoiceItemId4, RewardChoiceItemId5, RewardChoiceItemId6 FROM quest_template";
-ezCollections.QueryQuestRewardItemText = "";
+ezCollections.QueryQuestRewardItemText = "SELECT id, RewardItemId1, RewardItemId2, RewardItemId3, RewardItemId4, RewardChoiceItemId1, RewardChoiceItemId2, RewardChoiceItemId3, RewardChoiceItemId4, RewardChoiceItemId5, RewardChoiceItemId6 FROM quest_template";
+--ezCollections.QueryQuestRewardItemText = "";
 
 --查询数据库商人的售卖物品
 ezCollections.QueryNPCVendorItemText = "SELECT item FROM npc_vendor GROUP BY item";
